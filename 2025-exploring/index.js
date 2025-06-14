14bcf619ce7f1f402b38f1748682d31ba70bad8a
mtnleo <mtnleonardi@gmail.com>
2025-06-14 20:22:14 -0300

    Set a system for clasifying the cells and also changed the background

--- 2025-exploring/index.js
+++ 2025-exploring/index.js
@@ -1,9 +1,10 @@
 const cellSize = 32; // 2 rem
 
 function initCells() {
-    let cpw = window.innerWidth / cellSize;
-    let cph = window.innerHeight / cellSize;
+    let cpw = Math.floor(window.innerWidth / cellSize);
+    let cph = Math.floor(window.innerHeight / cellSize);
     let containerElement = document.querySelector(".container");
+    let x, y = 0;
 
     for(let i = 0; i < cph * cpw; i++) {
         
@@ -11,14 +12,26 @@ function initCells() {
         newElement.className = 'cell';
         newElement.addEventListener('click', () => {
             if(newElement.style.backgroundColor === 'white') {
-                newElement.style.backgroundColor = 'black';
+                newElement.style.backgroundColor = 'transparent';
             } 
             else {
                 newElement.style.backgroundColor = 'white';
             }
-        })
+        });
+        y = Math.floor((i / cpw));
+        x = i - (cpw * y);
+        newElement.id = `${x}-${y}`;
+
         containerElement?.appendChild(newElement);
     }
+}
+
+function checkCells() {
+
+}
+
+function run() {
+
 }
 
 window.addEventListener("DOMContentLoaded", () => {
